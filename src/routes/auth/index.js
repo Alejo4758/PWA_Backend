e76dd59e435570a88072d1b14cbc55a17514874e
FacuLedesmaBertalot <fanelebe.contacto@gmail.com>
2026-06-28 16:27:14 -0300

    fix: corregir nombre del endpoint de logout

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -4,6 +4,6 @@ import { login, logout } from '../../controllers/auth/index.js';
 const router = Router();
 
 router.post('/login', login);
-router.post('/register', logout);
+router.post('/logout', logout);
 
 export default router;
