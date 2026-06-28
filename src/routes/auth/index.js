c7199290a28356ca586747678b907e6f28d93513
Alejo <LopezAlejo0124@gmail.com>
2026-06-28 18:01:02 -0300

    Integración del Endpoint

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { login, logout } from '../../controllers/auth/index.js';
+import { login, logout, register } from '../../controllers/auth/index.js';
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/logout', logout);
+router.post('/register', register);
 
 export default router;
